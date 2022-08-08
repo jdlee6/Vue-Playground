20d59c3f2107df8f09ed77e4d74a1ac3d15c6465
Joe Lee <jdlee6@buffalo.edu>
2022-08-08 15:33:55 -0400

    Events

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ new Vue({
   data: {
     name: "Joe",
     job: "SWE",
+    age: 25,
     website: "https://www.imjoelee.com",
-    websiteTag: `<a href="https://www.imjoelee.com">Website</a>`
+    websiteTag: `<a href="https://www.imjoelee.com">Website</a>`,
+    x: 0,
+    y: 0
   },
 
   methods: {
     greet: function (time) {
       // this // refers to this Vue instance
       return 'Good ' + time + ' ' + this.name;
+    },
+    add: function (age) {
+      this.age += age
+    },
+    subtract: function (age) {
+      this.age -= age
+    },
+    updateXY: function (event) {
+      console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 })
