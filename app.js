c264be814dc5b90721c8b49f195ca934dac3a738
Joe Lee <jdlee6@buffalo.edu>
2022-08-09 15:23:13 -0400

    Punchbag game

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+    health: 100,
+    ended: false
   },
   methods: {
-
+    punch: function () {
+      this.health -= 10;
+      if (this.health === 0) {
+        this.ended = true;
+      }
+      console.log('health', this.health);
+    },
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    }
   },
   computed: {
 
