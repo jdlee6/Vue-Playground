bc8df797e25263daf53836cf478e2094dd306663
Joe Lee <jdlee6@buffalo.edu>
2022-08-09 16:45:09 -0400

    Intro to components

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-// Multiple Vue Instances
+// var data = {
+//   name: 'Yoshi'
+// }
+
+Vue.component('greeting', {
+  template: '<p>Hello there. This is {{ name }}. <button v-on:click="changeName">Change name</button></p>',
+  data: function () {
+    return {
+      name: 'Yoshi'
+    }
+  },
+  methods: {
+    changeName: function () {
+      this.name = 'Mario';
+    }
+  }
+})
+
 var one = new Vue({
   el: '#vue-app-one',
   data: {
-    title: 'Vue app one'
   },
   methods: {
-
   },
   computed: {
-    greet: function () {
-      return 'Hello from app one';
-    }
   }
 })
 
 var two = new Vue({
   el: '#vue-app-two',
   data: {
-    title: 'Vue app two'
+
   },
   methods: {
-    changeTitle: function () {
-      one.title = 'New title change!'
-    }
+
   },
   computed: {
-    greet: function () {
-      return 'Hello from app two';
-    }
+
   }
 })
 
